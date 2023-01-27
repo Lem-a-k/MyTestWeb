f777fc56bd424f1c0822e90dd02a30005a58fbdf
Lem <lilililili@yandex.ru>
2023-01-27 17:59:53 +0400

    Исторический музей

--- main.py
+++ main.py
@@ -1,44 +1,52 @@
 import requests
 import pygame
 
+
 def print_error(request_text, response):
     print("Ошибка выполнения запроса:")
     print(request_text)
     print(f"Http статус: {response.status_code} ({response.reason})")
     exit()
 
+
 apikey = "40d1649f-0493-4b70-98ba-98533de7710b"
-place = 'Саратов'
+place = 'Красная пл-дь, 1'
 geocoder_request = f"http://geocode-maps.yandex.ru/1.x/?apikey={apikey}&geocode={place}&format=json"
 response = requests.get(geocoder_request)
 if response:
     # Запрос успешно выполнен, печатаем полученные данные.
     # print(response.content)
     result = response.json()['response']['GeoObjectCollection']['featureMember'][0]['GeoObject']
+    # print(result)
+    address = result['metaDataProperty']['GeocoderMetaData']['Address']
+    full_address = f"{address['formatted']} ({address['postal_code']})"
     coordinates = result['Point']['pos']
+
+    print(full_address)
     print(coordinates)
 
-    map_request = f"http://static-maps.yandex.ru/1.x/?ll={','.join(coordinates.split())}&spn=0.005,0.005&l=sat"
-    response = requests.get(map_request)
-
-    if not response:
-        print_error(map_request, response)
-
-    # Запишем полученное изображение в файл.
-    map_file = "map.png"
-    with open(map_file, "wb") as file:
-        file.write(response.content)
-
-    # Инициализируем pygame
-    pygame.init()
-    screen = pygame.display.set_mode((600, 450))
-    # Рисуем картинку, загружаемую из только что созданного файла.
-    screen.blit(pygame.image.load(map_file), (0, 0))
-    # Переключаем экран и ждем закрытия окна.
-    pygame.display.flip()
-    while pygame.event.wait().type != pygame.QUIT:
-        pass
-    pygame.quit()
+    #
+    # map_request = f"http://static-maps.yandex.ru/1.x/?ll={','.join(coordinates.split())}&spn=0.005,0.005&l=sat"
+    # response = requests.get(map_request)
+    #
+    # if not response:
+    #     print_error(map_request, response)
+    #
+    # # Запишем полученное изображение в файл.
+    # map_file = "map.png"
+    # with open(map_file, "wb") as file:
+    #     file.write(response.content)
+    #
+    # # Инициализируем pygame
+    # pygame.init()
+    # screen = pygame.display.set_mode((600, 450))
+    # # Рисуем картинку, загружаемую из только что созданного файла.
+    # screen.blit(pygame.image.load(map_file), (0, 0))
+    # # Переключаем экран и ждем закрытия окна.
+    # pygame.display.flip()
+    # while pygame.event.wait().type != pygame.QUIT:
+    #     pass
+    # pygame.quit()
 
 else:
     print_error(geocoder_request, response)
