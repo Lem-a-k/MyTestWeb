473939f884c2920aed1fccf685fff96dc870d0ea
lem <lilililili@yandex.ru>
2023-01-31 17:27:59 +0400

    ЧЧФ и размер всех файлов

--- main.py
+++ main.py
@@ -1,10 +1,23 @@
-import sys
-from itertools import cycle
+import os
+import math
 
-try:
-    assert sys.argv[1:]
-    print(sum(c * int(x) for x, c in zip(sys.argv[1:], cycle([1, -1]))))
-except AssertionError:
-    print("NO PARAMS")
-except Exception as e:
-    print(e.__class__.__name__, e)
+BASE, UNITS = 1024, 'Б'
+POWERS = ['', 'К', 'М', 'Г']
+
+
+def human_read_format(size):
+    power = int(math.floor(math.log(size, BASE) if size else 0))
+    w = size / BASE ** power
+    return f'{round(w)}{POWERS[power] + UNITS}'
+
+
+def get_files_sizes():
+    lines = []
+    for item in os.listdir():  # path=os.getcwd()
+        if os.path.isfile(item):
+            lines.append(f'{item} {human_read_format(os.path.getsize(item))}')
+    return '\n'.join(lines)
+
+
+if __name__ == '__main__':
+    print(get_files_sizes())
