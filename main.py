22128cc5260ac8e8db8efc01343757546115d5d7
lem <lilililili@yandex.ru>
2023-02-03 17:03:38 +0400

    lines with parameter

--- main.py
+++ main.py
@@ -1,24 +1,3 @@
-import argparse
+from get_lines import count_lines
 
-parser = argparse.ArgumentParser()
-parser.add_argument('--barbie', type=int, default=50)
-parser.add_argument('--cars', type=int, default=50)
-parser.add_argument('--movie', choices=['melodrama', 'football', 'other'], default='other')
-
-margs = parser.parse_args()
-
-if margs.barbie > 100 or margs.barbie < 0:
-    margs.barbie = 50
-
-if margs.cars > 100 or margs.cars < 0:
-    margs.cars = 50
-
-margs.movie = (0 if margs.movie == 'melodrama'
-               else (100 if margs.movie == 'football' else 50))
-boy = int((100 - margs.barbie + margs.cars + margs.movie) / 3)
-girl = 100 - boy
-
-print(f"""boy: {boy}
-girl: {girl}
-""")
-
+print(count_lines('test.json'))
