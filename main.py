fe281d2030cabe10a5570cb4759e291099cb9df6
lem <lilililili@yandex.ru>
2023-02-03 16:49:01 +0400

    barbie+cars

--- main.py
+++ main.py
@@ -9,4 +9,16 @@ margs = parser.parse_args()
 
 if margs.barbie > 100 or margs.barbie < 0:
     margs.barbie = 50
-print(margs.barbie)
+
+if margs.cars > 100 or margs.cars < 0:
+    margs.cars = 50
+
+margs.movie = (0 if margs.movie == 'melodrama'
+               else (100 if margs.movie == 'football' else 50))
+boy = int((100 - margs.barbie + margs.cars + margs.movie) / 3)
+girl = 100 - boy
+
+print(f"""boy: {boy}
+girl: {girl}
+""")
+
