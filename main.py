98672bd180c0659d60478adc4fe1bdd7a9e3aef2
lem <lilililili@yandex.ru>
2023-02-03 17:21:08 +0400

    get request with params

--- main.py
+++ main.py
@@ -1,3 +1,54 @@
-from get_lines import count_lines
+import requests
+import pygame
 
-print(count_lines('test.json'))
+def print_error(request_text, response):
+    print("Ошибка выполнения запроса:")
+    print(request_text)
+    print(f"Http статус: {response.status_code} ({response.reason})")
+    exit()
+
+apikey = "40d1649f-0493-4b70-98ba-98533de7710b"
+place = 'Саратов'
+api_server = "http://geocode-maps.yandex.ru/1.x/"
+params = {
+    'apikey': apikey,
+    'geocode': place,
+    'format': 'json'
+}
+response = requests.get(api_server, params)
+if response:
+    # Запрос успешно выполнен, печатаем полученные данные.
+    # print(response.content)
+    result = response.json()['response']['GeoObjectCollection']['featureMember'][0]['GeoObject']
+    coordinates = result['Point']['pos']
+    print(coordinates)
+
+    api_server = "http://static-maps.yandex.ru/1.x/"
+    params = {
+        'll': ','.join(coordinates.split()),
+        'spn': '0.005,0.005',
+        'l': 'sat'
+    }
+    response = requests.get(api_server, params)
+
+    if not response:
+        print_error("maps request", response)
+
+    # Запишем полученное изображение в файл.
+    map_file = "map.png"
+    with open(map_file, "wb") as file:
+        file.write(response.content)
+
+    # Инициализируем pygame
+    pygame.init()
+    screen = pygame.display.set_mode((600, 450))
+    # Рисуем картинку, загружаемую из только что созданного файла.
+    screen.blit(pygame.image.load(map_file), (0, 0))
+    # Переключаем экран и ждем закрытия окна.
+    pygame.display.flip()
+    while pygame.event.wait().type != pygame.QUIT:
+        pass
+    pygame.quit()
+
+else:
+    print_error("geocoder request", response)
