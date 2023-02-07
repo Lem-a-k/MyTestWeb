8be2ad553f23e5f0b6ef85f3ac26291146fc5e65
lem <lilililili@yandex.ru>
2023-02-07 17:10:58 +0400

    Заготовка большой задачи 0.2

--- main.py
+++ main.py
@@ -1,6 +1,45 @@
-from mapapi_PG import show_map
+import sys
+import os
 
-coordinates = '151.21529330927066,-33.85653004033911'
-z = 17
+import pygame
 
-show_map(coordinates, map_type='sat', add_params={'z': z})
+from mapapi import get_map
+
+
+def main():
+    coordinates = '151.21529330927066,-33.85653004033911'
+    z = 17
+    response = get_map(coordinates, map_type='sat', add_params={'z': z})
+
+    if not response:
+        print("Ошибка выполнения запроса:")
+        print(response.url)
+        print("Http статус:", response.status_code, "(", response.reason, ")")
+        sys.exit(1)
+
+    # Запишем полученное изображение в файл.
+    map_file = "map.png"
+    try:
+        with open(map_file, "wb") as file:
+            file.write(response.content)
+    except IOError as ex:
+        print("Ошибка записи временного файла:", ex)
+        sys.exit(2)
+
+    # Инициализируем pygame
+    pygame.init()
+    screen = pygame.display.set_mode((600, 450))
+    # Рисуем картинку, загружаемую из только что созданного файла.
+    screen.blit(pygame.image.load(map_file), (0, 0))
+    # Переключаем экран и ждем закрытия окна.
+    pygame.display.flip()
+    while pygame.event.wait().type != pygame.QUIT:
+        pass
+
+    pygame.quit()
+    # Удаляем за собой файл с изображением.
+    os.remove(map_file)
+
+
+if __name__ == '__main__':
+    main()
