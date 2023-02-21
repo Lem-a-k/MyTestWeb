056c5169d9e4c0dd432997f06c1b07a37bedcaa1
lem <lilililili@yandex.ru>
2023-02-21 17:54:33 +0400

    bootstrap navbar

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, url_for
 
 app = Flask(__name__)
 
@@ -6,7 +6,69 @@ app = Flask(__name__)
 @app.route('/')
 @app.route('/index')
 def main_page():
-    return "<h1>Привет, Яндекс!</h1>"
+    return "<h1>Привет, Яндекс!</h1><br />Ура!"
+
+
+@app.route('/second')
+def second_page():
+    return """Second <p>page</p>
+1
+<div>2</div>"""
+
+
+@app.route('/countdown')
+def countdown():
+    countdown_list = [str(x) for x in range(10, 0, -1)]
+    countdown_list.append('Пуск!')
+    return '</br>'.join(countdown_list)
+
+
+@app.route('/image_sample')
+def image():
+    return f'''<img width=500pt src="{url_for('static', filename='img/owl.jpeg')}" 
+           alt="та самая сова">'''
+
+
+@app.route('/sample_page')
+def return_sample_page():
+    return f"""<!doctype html>
+                <html lang="en">
+                  <head>
+                    <meta charset="utf-8">
+                    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
+                    <link rel="stylesheet" type="text/css" href="{url_for('static', filename='css/styles.css')}" />                    
+                    <title>Привет, Яндекс!</title>
+                  </head>
+                  <body>
+<nav class="navbar navbar-expand-lg bg-body-tertiary">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">Navbar</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNav">
+      <ul class="navbar-nav">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="index">Home</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="/image_sample">image_sample</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Pricing</a>
+        </li>
+      </ul>
+    </div>
+  </div>
+</nav>
+                  <h1>"Правильная" HTML-страница</h1>
+                  <p>(made in YAL)</p>
+                  <div class="alert alert-primary" role="alert">
+                      А мы тут компонентами Bootstrap балуемся
+                  </div>
+                    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script>
+                </body>
+            </html>"""
 
 
 if __name__ == '__main__':
