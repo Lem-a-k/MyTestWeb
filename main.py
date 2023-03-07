7d1362e467a65ac464a4e629c08dc22191caa9a6
lem <lilililili@yandex.ru>
2023-03-07 17:53:25 +0400

    flaskwtf

--- main.py
+++ main.py
@@ -1,8 +1,28 @@
-from flask import Flask, url_for, request, render_template
+from flask import Flask, url_for, request, render_template, redirect
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, BooleanField, SubmitField
+from wtforms.validators import DataRequired
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
 
 user = "Ученик Лицея Академии Яндекса"
+
+
+class LoginForm(FlaskForm):
+    username = StringField('Логин', validators=[DataRequired()])
+    password = PasswordField('Пароль', validators=[DataRequired()])
+    remember_me = BooleanField('Запомнить меня')
+    submit = SubmitField('Войти')
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        return redirect('/index')
+    return render_template('login.html', title='Авторизация', form=form,
+                           css_file=url_for('static', filename='css/styles.css'))
 
 
 @app.route('/')
