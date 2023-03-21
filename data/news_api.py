a1c621eb05774e9616b964ae6140c3a563b863ea
lem <lilililili@yandex.ru>
2023-03-21 17:48:58 +0400

    api add news

--- data/news_api.py
+++ data/news_api.py
@@ -1,5 +1,5 @@
 import flask
-from flask import jsonify
+from flask import jsonify, request
 
 from . import db_session
 from .news import News
@@ -36,3 +36,22 @@ def get_one_news(news_id):
                 'id', 'title', 'content', 'user.id', 'user.name', 'is_private'))
         }
     )
+
+
+@blueprint.route('/api/news', methods=['POST'])
+def create_news():
+    if not request.json:
+        return jsonify({'error': 'Empty request'})
+    elif not all(key in request.json for key in
+                 ['title', 'content', 'user_id', 'is_private']):
+        return jsonify({'error': 'Bad request'})
+    db_sess = db_session.create_session()
+    news = News(
+        title=request.json['title'],
+        content=request.json['content'],
+        user_id=request.json['user_id'],
+        is_private=request.json['is_private']
+    )
+    db_sess.add(news)
+    db_sess.commit()
+    return jsonify({'success': 'OK'})
